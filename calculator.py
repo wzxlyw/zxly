c4136ae028827e00b3a91b1439ee04a0fa64c63b
wzxlyw <88080018+wzxlyw@users.noreply.github.com>
2021-07-28 12:02:25 +0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,19 @@
+vopros = input(" +,-? ")
+
+# +
+
+if vopros == "+":
+	pervoe = int(input(" Enter first number: "))
+	vtoroe = int(input(" Enter second number: "))
+	c = pervoe + vtoroe
+	print(" Answer: " + str(c))
+	input("")
+
+# -
+
+if vopros == "-":
+	pervoe = int(input(" Enter first number: "))
+	vtoroe = int(input(" Enter second number: "))
+	c = pervoe - vtoroe
+	print(" Answer: " + str(c))
+	input("")
